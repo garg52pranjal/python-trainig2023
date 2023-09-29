d0af8cf503b176644978b993647134dbff7b223e
garg52pranjal <146409975+garg52pranjal@users.noreply.github.com>
2023-09-29 11:42:24 +0530

    day 1 complete

---
+++ day1.py
@@ -0,0 +1,14 @@
+print(100)
+print('hello word')
+print("hey there")
+#print(23-7)
+x=8
+a=5.5
+z="hello world"
+print(x,a,z) 
+# printing tpye of variable 
+print(type(z))
+# maths ops 
+sum1=x+z
+mull=x+a
+print(sum1,mull)
